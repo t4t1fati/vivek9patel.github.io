1e4dd083128be528549baa78a487d9c222a6d523
t4t1fati <fatipz1@gmail.com>
2023-06-11 01:01:57 +0200

    Update spotify.js

--- components/apps/spotify.js
+++ components/apps/spotify.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Spotify() {
     return (
-        <iframe src="https://www.dextools.io/app/ether/pair-explorer/0x2e0adb1526a091e83d030e059455b7c5eb0d5d69" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
+        <iframe src="https://www.dextools.io" frameBorder="0" title="Spotify" className="h-full w-full bg-ub-cool-grey"></iframe>
     )
 }
 
